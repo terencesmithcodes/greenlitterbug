8f86e908acfdedc548438ed910dc5d0c66f9d134
Terence Smith <terencesmith1@me.com>
2023-06-11 23:00:34 -0400

    Added browseer router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Dashboard from './pages/Dashboard'
+import SignIn from './pages/SignIn'
+import Register from './pages/Register'
+import LandingPage from './pages/LandingPage'
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Green Liter Bug Landing</h1>
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<SignIn />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/register" element={<Dashboard />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   )
 }
 
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function SignIn() {
+  return <div>this is a test</div>
+}
+
+export default SignIn
